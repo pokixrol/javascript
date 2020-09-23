8211c536e38205c2709fe3ed375c5b12a736caa7
student <kaja.ricna@seznam.cz>
2020-09-23 11:08:28 +0200

    První commit

---
+++ 01/js/muj.js
@@ -0,0 +1,30 @@
+//alert ("toto pochází z externího souboru");
+//var nadpis - starší delkarace proměnné js
+//proměnná v nových verzích js
+let nadpis = document.getElementById('nadpis');
+//nadpis.innerText = 'Ahoj';
+
+
+/*nadpis.addEventListener('click', function() {
+nadpis.innerHTML = 'Ahoj <i>Karle</i>';
+nadpis.style.color = 'blue';
+nadpis.style.fontFamily =  'serif';
+nadpis.classList.add('text-danger');
+nadpis.classList.remove('text-uppercase');
+});*/
+
+
+/*let main = document.getElementById('main');
+
+main.addEventListener('mouseover', function() {
+    main.innerHTML = '<b>Warning!</b>';
+    main.style.fontFamily =  'serif';
+    main.style.backgroundColor =  'blue';
+    main.classList.add('text-danger');
+    main.classList.add('display-1');
+    main.classList.add('text-uppercase');
+    main.classList.add('text-center');
+    main.classList.remove('container');
+    });*/
+
+
